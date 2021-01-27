deb5c3a5963930a6e77c69586aaf338b8254e2a2
zhangmengfei <>
2021-01-27 18:48:52 +0800

    add checkInvitationCode

---
+++ algorithm/New/CheckInvitationCode.php
@@ -0,0 +1,42 @@
+<?php
+
+function getCheckRes(string $str, int $len)
+{
+    if (strlen($str) != $len) {
+        return false;
+    } else {
+
+        //是否小写字母和数组组成
+        $get_info = preg_match('/[a-z0-9]+$/', $str);
+
+        if ($get_info) {
+            $str = strrev($str);
+
+            $sum = 0;
+
+            for ($i = 0; $i < $len; $i++) {
+                if (($i + 1) % 2 == 1) {
+                    $sum += $str[$i];
+                } else {
+                    if (intval($str[$i])) {
+                        $sum += $str[$i];
+                    } else {
+                        $ascii_info = (ord($str[$i]) - ord('a')) % 9 +1;
+                        $sum += $ascii_info;
+                    }
+                }
+            }
+
+            if($sum%10 == 0){
+                return true;
+            }else{
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+}
+
+var_dump(getCheckRes('19191919191919a9',16));
